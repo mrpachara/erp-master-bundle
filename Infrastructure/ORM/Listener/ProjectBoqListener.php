c8c8b5f85e1b8cfb55af8bbbb49df68a57d55805
Pachara Tinamas <mrpachara@gmail.com>
2023-04-01 04:23:26 +0700

    FIX: reorder boq position

--- Infrastructure/ORM/Listener/ProjectBoqListener.php
+++ Infrastructure/ORM/Listener/ProjectBoqListener.php
@@ -8,45 +8,49 @@ use Erp\Bundle\MasterBundle\Entity\Project;
 use Erp\Bundle\MasterBundle\Entity\ProjectBoqData;
 use Erp\Bundle\MasterBundle\Entity\ProjectBoq;
 
-class ProjectBoqListener {
-  protected function assignBudgetType(ProjectBoqData $entity, array $budgetTypes, EntityManagerInterface $em) {
-    foreach($entity->getBudgets() as $key => $budget) {
-      if(array_key_exists($key, $budgetTypes)) {
-        $budget->setBoqBudgetType($budgetTypes[$key]);
-        $budget->setBoqData($entity);
-      } else {
-        $entity->removeBudget($budget);
-        if(!empty($budget->getBoqBudgetType())) $em->remove($budget->getBoqBudgetType());
-      }
-    }
-  }
-
-  protected function assignDeep(ProjectBoqData $entity, array $budgetTypes, EntityManagerInterface $em) {
-    foreach($entity->getChildren() as $position => $child) {
-      if(!empty($child->getId()) && ($entity !== $child->getParent()))
-        throw new \Exception("Invalid parent child relation!!!");
-
-      $child->setParent($entity);
-      $child->setPosition($position);
+class ProjectBoqListener
+{
+    protected function assignBudgetType(ProjectBoqData $entity, array $budgetTypes, EntityManagerInterface $em)
+    {
+        foreach ($entity->getBudgets() as $key => $budget) {
+            if (array_key_exists($key, $budgetTypes)) {
+                $budget->setBoqBudgetType($budgetTypes[$key]);
+                $budget->setBoqData($entity);
+            } else {
+                $entity->removeBudget($budget);
+                if (!empty($budget->getBoqBudgetType())) $em->remove($budget->getBoqBudgetType());
+            }
+        }
     }
 
-    $this->assignBudgetType($entity, $budgetTypes, $em);
+    protected function assignDeep(ProjectBoqData $entity, array $budgetTypes, EntityManagerInterface $em)
+    {
+        foreach ($entity->getChildren() as $position => $child) {
+            if (!empty($child->getId()) && ($entity !== $child->getParent()))
+                throw new \Exception("Invalid parent child relation!!!");
 
-    foreach($entity->getChildren() as $child) {
-      $this->assignDeep($child, $budgetTypes, $em);
+            $child->setParent($entity);
+            //$child->setPosition($position);
+        }
+
+        $this->assignBudgetType($entity, $budgetTypes, $em);
+
+        foreach ($entity->getChildren() as $child) {
+            $this->assignDeep($child, $budgetTypes, $em);
+        }
     }
-  }
 
-  public function preFlush(ProjectBoq $entity, PreFlushEventArgs $event) {
-    $budgetTypes = [];
-    foreach($entity->getBudgetTypes() as $budgetType) {
-      if(!empty($budgetType->getId()) && ($entity !== $budgetType->getBoq()))
-        throw new \Exception("Invalid budgetType relation!!!");
+    public function preFlush(ProjectBoq $entity, PreFlushEventArgs $event)
+    {
+        $budgetTypes = [];
+        foreach ($entity->getBudgetTypes() as $budgetType) {
+            if (!empty($budgetType->getId()) && ($entity !== $budgetType->getBoq()))
+                throw new \Exception("Invalid budgetType relation!!!");
 
-      $budgetType->setBoq($entity);
-      $budgetTypes[$budgetType->get_code()] = $budgetType;
+            $budgetType->setBoq($entity);
+            $budgetTypes[$budgetType->get_code()] = $budgetType;
+        }
+
+        $this->assignDeep($entity, $budgetTypes, $event->getEntityManager());
     }
-
-    $this->assignDeep($entity, $budgetTypes, $event->getEntityManager());
-  }
 }
